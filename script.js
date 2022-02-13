a672466bba57a1f44ae1e37cb3d3aa93d3de820f
Vinay <vinay.kumar@firewires.in>
2022-02-13 23:53:26 +0530

    added work experience project

--- script.js
+++ script.js
@@ -1,50 +1,116 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 60){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+  type() {
+    const current = this.wordIndex % this.words.length;
+    const fullTxt = this.words[current];
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // Initial Type Speed
+    let typeSpeed = 100;
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      typeSpeed = this.wait;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.wordIndex++;
+      typeSpeed = 100;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+$(document).ready(function () {
+  const txtElement = document.querySelector(".text-typewriter");
+  const words = ["Frontend Developer", "React Native Developer", "JavaScript Developer"];
+  const wait = "2000";
+  const openEl = document.querySelector("[data-open]");
+  const closeEl = document.querySelector("[data-close]");
+  const navbar = document.querySelector(".navbar");
+  const isVisible = "is-visible";
+
+  new TypeWriter(txtElement, words, wait);
+
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 60) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  openEl?.addEventListener("click", function () {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+    navbar.classList.add("display-none");
+  });
+
+  closeEl?.addEventListener("click", function () {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    navbar.classList.remove("display-none");
+  });
+
+  document.addEventListener("click", (e) => {
+    console.log("clicked");
+    if (e.target == document.querySelector(".modal.is-visible")) {
+      document.querySelector(".modal.is-visible").classList.remove(isVisible);
+      navbar.classList.remove("display-none");
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    // if we press the ESC
+    console.log("escape");
+    if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+      document.querySelector(".modal.is-visible").classList.remove(isVisible);
+      navbar.classList.remove("display-none");
+    }
+  });
 });
